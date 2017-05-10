da9c4f6c5bfde962036462caf1b5cae0bcf4a5f8
omyono <heri.cahyono@students.amikom.ac.id>
2017-05-10 19:32:16 +0700

    mengisi konten

---
+++ js/custom-animasi.js
@@ -0,0 +1,103 @@
+
+$(document).ready(function(){
+
+  $('.hideN').hide();
+
+  $('#satu').mouseenter(function(){
+    $('#tokosatu').delay("fast").slideDown()
+  });
+
+  $('#satu').mouseleave(function(){
+    $('#tokosatu').slideUp()
+  });
+
+    $('#dua').mouseenter(function(){
+    $('#toko2').delay("fast").slideDown()
+    });
+
+    $('#dua').mouseleave(function(){
+      $('#toko2').slideUp()
+    });
+
+  $('#tiga').mouseenter(function(){
+    $('#toko3').delay("fast").slideDown()
+  });
+
+  $('#tiga').mouseleave(function(){
+    $('#toko3').slideUp()
+  });
+
+    $('#empat').mouseenter(function(){
+    $('#toko4').delay("fast").slideDown()
+    });
+
+    $('#empat').mouseleave(function(){
+      $('#toko4').slideUp()
+    });
+
+  $('#lima').mouseenter(function(){
+    $('#toko5').delay("fast").slideDown()
+  });
+
+  $('#lima').mouseleave(function(){
+    $('#toko5').slideUp()
+  });
+
+    $('#enam').mouseenter(function(){
+    $('#toko6').delay("fast").slideDown()
+    });
+
+    $('#enam').mouseleave(function(){
+      $('#toko6').slideUp()
+    });
+
+  $('#tuju').mouseenter(function(){
+    $('#toko7').delay("fast").slideDown()
+  });
+
+  $('#tuju').mouseleave(function(){
+    $('#toko7').slideUp()
+  });
+
+    $('#delapan').mouseenter(function(){
+    $('#toko8').delay("fast").slideDown()
+    });
+
+    $('#delapan').mouseleave(function(){
+      $('#toko8').slideUp()
+    });
+
+  $('#sembilan').mouseenter(function(){
+    $('#toko9').delay("fast").slideDown()
+  });
+
+  $('#sembilan').mouseleave(function(){
+    $('#toko9').slideUp()
+  });
+
+    $('#sepuluh').mouseenter(function(){
+    $('#toko10').delay("fast").slideDown()
+    });
+
+    $('#sepuluh').mouseleave(function(){
+      $('#toko10').slideUp()
+    });
+
+  $('#sebelas').mouseenter(function(){
+    $('#toko11').delay("fast").slideDown()
+  });
+
+  $('#sebelas').mouseleave(function(){
+    $('#toko11').slideUp()
+  });
+
+    $('#duabelas').mouseenter(function(){
+    $('#toko12').delay("fast").slideDown()
+    });
+
+    $('#duabelas').mouseleave(function(){
+      $('#toko12').slideUp()
+    });
+
+
+});
